f55878c43b3499444af0fbe80752ef635eb96425
punpnx <87162439+punpnx@users.noreply.github.com>
2024-01-17 00:00:52 +0700

    Add files via upload

---
+++ ex2/Subscriber/src/java/subscriber/Main.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package subscriber;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+/**
+ *
+ * @author Puntawan Buasung
+ */
+public class Main {
+    @Resource(mappedName = "jms/SimpleJMSQueue")
+    private static Queue queue;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Connection connection = null;
+        Session session = null;
+        Destination dest = null;
+        MessageConsumer consumer = null;
+        TextListener listener = null;
+        TextMessage message = null;
+        InputStreamReader inputStreamReader = null;
+        char answer = '\0';
+        
+        try {
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            dest = topic;
+            consumer = session.createConsumer(dest);
+            listener = new TextListener();
+            consumer.setMessageListener(listener);
+            connection.start();
+            System.out.println(
+                    "To end program, type Q or q, " + "then <return>");
+            inputStreamReader = new InputStreamReader(System.in);
+
+            while (!((answer == 'q') || (answer == 'Q'))) {
+                try {
+                    answer = (char) inputStreamReader.read();
+                } catch (IOException e) {
+                    System.err.println("I/O exception: " + e.toString());
+                }
+            }
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            }
+        }
+    }
+    
+}
